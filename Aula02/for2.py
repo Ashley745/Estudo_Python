c4c0c22faf9444fe216e3d809f6445a5e129fba4
Ashley Santana <ashleysantana053@gmail.com>
2023-11-29 10:50:06 -0300

    Estrutura de repetição for e while

---
+++ Aula02/for2.py
@@ -0,0 +1,10 @@
+# Variável de contagem
+
+qtd = 0
+for i in range(1,201):
+    if i % 2 == 0:
+        qtd = qtd + 1
+print("A quantidade de números pares entr 1 e 200 é ") 
+print(qtd)
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
---
+++ Aula02/for4.py
@@ -0,0 +1,8 @@
+# Variável de contagem
+
+qtd = 0 
+for i in range (1973,2023):
+    if i % 4 == 0:
+        qtd = qtd + 1
+print("A quantidade de ano bissextos são ")
+print(qtd)
