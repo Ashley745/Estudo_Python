de2afb9a198d1474836fc453790e7847b1a10360
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 09:53:42 -0300

    Trabalhando com desvio de fluxo if

---
+++ Revisao/desvio1.py
@@ -0,0 +1,13 @@
+# Este programa obtem o nome do aluno e a nota média
+# Se o aluno tiver uma média acima ou igual a 6
+# estará aprovado; caso contrário reprovado
+
+nome = input("Digite o nome do aluno: ")
+media = input("Digite a nota média do aluno: ")
+media= float(media)
+
+if(media>=6):
+    print("Aprovado")
+else:
+    print("Reprovado")
+    
